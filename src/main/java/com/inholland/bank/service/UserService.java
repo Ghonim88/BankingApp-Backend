bf4f0fe26b7619a33fb56332b1e9cd610cb9149c
Abdelrahman Ghonim <bodyghonim345@gmail.com>
2025-06-09 11:33:26 +0200

    cleaning code, final checks, modifing testing

--- src/main/java/com/inholland/bank/service/UserService.java
+++ src/main/java/com/inholland/bank/service/UserService.java
@@ -32,25 +32,6 @@ public class UserService {
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
   }
-////  public Optional<User> login(String email, String password) {
-////    // Fetch customer by email
-////    Optional<User> userOptional = userRepository.findByEmail(email);
-////
-////    if (userOptional.isPresent()) {
-////      User user = userOptional.get();
-////
-////      // Check if the password matches
-////      if (passwordEncoder.matches(password, user.getPassword())) {
-////        // Password matches, return the customer
-////        return Optional.of(user);
-////      } else {
-////        // Password doesn't match
-////        return Optional.empty();
-////      }
-////    }
-//    // Customer not found
-//    return Optional.empty();
-//  }
   public User findByEmail(String email) {
     return userRepository.findByEmail(email).orElse(null);
   }
@@ -60,7 +41,6 @@ public class UserService {
 
   public Object getLoggedInUser(User user) {
     Long userId = user.getUserId();
-   // String role = user.getUserRole().name();
 
     switch (user.getUserRole()) {
       case CUSTOMER -> {
